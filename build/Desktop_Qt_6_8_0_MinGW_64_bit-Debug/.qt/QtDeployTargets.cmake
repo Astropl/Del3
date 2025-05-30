525e1b13fcddb8849fe09f25fc0d7c5726ef0b03
Paweł Martys <pmartys@acm.lan>
2025-05-30 11:14:14 +0200

    Usuń build z repo, zgodnie z .gitignore

--- build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_Del3_FILE C:/Users/pmartys/Documents/QT Projects/Del3/build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/Del3.exe)
-set(__QT_DEPLOY_TARGET_Del3_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_Del3_RUNTIME_DLLS C:/Qt/6.8.0/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.8.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.8.0/mingw_64/bin/Qt6Core.dll)
