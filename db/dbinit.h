1cb5f3fb9930bb4855c72516474cc7f3a9618c4b
Astropl <martys.pawel@gmail.com>
2025-05-31 14:14:21 +0200

    obsluga db

---
+++ db/dbinit.h
@@ -0,0 +1,15 @@
+#ifndef DBINIT_H
+#define DBINIT_H
+
+#include <QMainWindow>
+
+class DbInit : public QMainWindow
+{
+    Q_OBJECT
+public:
+    explicit DbInit(QWidget *parent = nullptr);
+
+signals:
+};
+
+#endif // DBINIT_H
